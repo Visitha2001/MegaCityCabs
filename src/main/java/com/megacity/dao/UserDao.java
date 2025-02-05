528a8f9873d5f1d3d570f5261cadddfb7dc56da8
Visitha Nirmal Rajapaksha <visitha2001@gmail.com>
2025-02-05 14:23:11 +0530

    login register

--- src/main/java/com/megacity/dao/UserDao.java
+++ src/main/java/com/megacity/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.megacity.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.ResultSet;
 import com.megacity.models.User;
 import com.megacity.utils.DBConnection;
 
@@ -28,6 +29,23 @@ public class UserDao {
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             return false; // Return false if the JDBC driver is not found
+        }
+    }
+    
+    // Method to authenticate a user
+    public boolean authenticateUser(String usernameOrEmail, String password) {
+        String query = "SELECT * FROM users WHERE (username = ? OR email = ?) AND password = ?";
+        try (Connection connection = DBConnection.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+            preparedStatement.setString(1, usernameOrEmail);
+            preparedStatement.setString(2, usernameOrEmail);
+            preparedStatement.setString(3, password);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            return resultSet.next(); // Return true if a matching record exists
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return false; // Return false if an exception occurs
         }
     }
 }
